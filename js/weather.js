a35a0d0735e708e30ff7994a3cd49d2c69746446
Brian Zimmerman <briangzimmerman@gmail.com>
2018-12-24 12:59:14 -0600

    adding more stuff for the future

---
+++ js/weather.js
@@ -0,0 +1,16 @@
+function Weather(zip) {
+    this.zip = zip;
+    this.weather = undefined;
+}
+
+Weather.prototype.update = function() {
+
+};
+
+Weather.prototype.getTemperature = function() {
+
+};
+
+Weather.prototype.getWeather = function() {
+
+};
