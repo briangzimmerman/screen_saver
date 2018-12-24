f8f9d398f85cc8358437b14f3e8ec9dc22d0ee1d
Brian Zimmerman <briangzimmerman@gmail.com>
2018-12-24 12:49:36 -0600

    adding some things that will be necessary later

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 var $date = $('#date');
 var $time = $('#time');
 
+updateBackground();
+
+setTimeout(function() {
+    updateBackground();
+    setInterval(updateBackground, 60000);
+}, (60 - new Date.getMinutes) * 60000);
+
 
 setInterval(function() {
     $date.text(moment().format('dddd, MMMM Do'));
